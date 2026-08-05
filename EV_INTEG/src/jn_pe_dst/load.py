5770263d1e0cbdb6ea2437e2c4f4d2550b66c853
Matevž Vidovič <matevz.vidovic@flycom.si>
2026-08-05 15:19:39 +0200

    a

--- EV_INTEG/src/jn_pe_dst/load.py
+++ EV_INTEG/src/jn_pe_dst/load.py
@@ -4,10 +4,23 @@ import sys
 sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
 from ev_backfill import TableSpec, run_loader
 
-SPEC = TableSpec("jn_pe_dst", "JN_PE_DST", "ID_PE_DST", (
-    "ID_PE_DST", "ID_PE", "DST_SID", "JN_REV_NUM", "JN_REV_NUM_TO", "JN_REV_NUM_PREV",
-    "JN_STATUS", "VREDNOST_DODATEK", "PODATKI", "DELITEV_MSG",
-))
+SPEC = TableSpec(
+    "jn_pe_dst",
+    "JN_PE_DST",
+    "ID_PE_DST",
+    (
+        "ID_PE_DST",
+        "ID_PE",
+        "DST_SID",
+        "JN_REV_NUM",
+        "JN_REV_NUM_TO",
+        "JN_REV_NUM_PREV",
+        "JN_STATUS",
+        "VREDNOST_DODATEK",
+        "PODATKI",
+        "DELITEV_MSG",
+    ),
+)
 
 if __name__ == "__main__":
     run_loader(SPEC, __file__)
